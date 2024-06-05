d536330758b626f89cfb99e19df796857727af0d
Moosa <computerknowledge817@gmail.com>
2024-06-05 15:04:05 +0500

    new file:   config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_TITLE: str = "GOOFCOIN API"
+    API_DESCRIPTION: str = (
+        "A simple API to interact with the GoofCoin prototype cryptocurrency"
+    )
+    API_VERSION: str = "1.0.0"
+
+    SECRET_KEY: str
+    DEBUG: bool
+    DATABASE_URL: str
+    ALLOWED_HOSTS: list = ["*.vercel.app", "localhost", "127.0.0.1"]
+
+    class Config:
+        env_file = ".env"
+
+
+config = Settings()
