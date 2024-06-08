eb997a53a533946bebc1aee2842ea533e1099ae6
Moosa <computerknowledge817@gmail.com>
2024-06-08 22:32:06 +0500

    added tailwindcss

---
+++ reactapp/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#ffffff',
+        'background': '#313c87',
+        'primary': '#50acf7',
+        'secondary': '#144185',
+        'accent': '#74f2cc',
+      },
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Open Sans',
+        body: 'Poppins',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
+  },
+  plugins: [],
+
+}
