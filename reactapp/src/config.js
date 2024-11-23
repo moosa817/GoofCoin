30ae4474f5017ec3c8d7a91862aab5e9ae80a855
Muhammad Moosa <computerknowledge817@gmail.com>
2024-11-23 13:52:10 +0500

    updated api origin

--- reactapp/src/config.js
+++ reactapp/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // API_URL: "https://goofcoin.vercel.app",
-    API_URL: "http://localhost:8000",
+    API_URL: "https://goofcoin.vercel.app",
+    // API_URL: "http://localhost:8000",
     APP_NAME: "GoofCoin",
     DEFAULT_LANGUAGE: "en",
 };
